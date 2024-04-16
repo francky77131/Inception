71e4deb3141cab40d1dc071308b8f060403c18d2
francky77131 <franck.gojard@gmail.com>
2024-04-16 17:20:13 +0200

    o

--- srcs/requirements/wordpress/conf/conf.sh
+++ srcs/requirements/wordpress/conf/conf.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-sleep 5
+sleep 15
 
-if [ ! -f "/var/www/html/wp-config.php" ]
+if [ ! -f "/var/www/wordpress/wp-config.php" ]
 then
-	wp core download --path=/var/www/html --allow-root --force
+	wp core download --path=/var/www/wordpress --allow-root --force
 	
-	cd /var/www/html
+	cd /var/www/wordpress
 	
 	wp config create --dbname=$SQL_DATABASE \
 			 --dbuser=$SQL_USER \
 			 --dbpass=$SQL_PASSWORD \
 			 --allow-root \
-			 --dbhost=mariadb:3306 --path='/var/www/html'
+			 --dbhost=mariadb:3306 --path='/var/www/wordpress'
 
 	wp core install	--title=$WP_TITLE \
 			--admin_user=$WP_ADMIN \
